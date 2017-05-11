28ec2ed53fdefc3e75540e176b16ee24c001e3ba
sbrycebarker <sbrycesti@gmail.com>
2017-05-11 09:51:45 -0600

    build

--- server/models.js
+++ server/models.js
@@ -4,9 +4,19 @@ var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
   name: { type: String},
+  package: { type: String },
   imgurl: {type: String},
   description: { type: String},
-  price: {type: String}
+  price: {type: String},
+  mpg: {type: Number},
+  enginesize: {type: Number},
+  awd: Boolean,
+  dat: Boolean,
+  bsd: Boolean,
+  cvt: Boolean,
+  aaw: Boolean,
+  gps: Boolean,
+  ltu: Boolean,
 });
 
 module.exports = mongoose.model('cars', ProductSchema);
