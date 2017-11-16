2f06dad524835ae62294c8af4042fab3d4372549
sbrycebarker <sbrycesti@gmail.com>
2017-11-15 22:27:17 -0700

    frontedit

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -211,7 +211,6 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
-        nav: true,
         autoplay: 10000,
         items: 1,
         autoHeight: true
--- public/js/jquery.js
+++ public/js/jquery.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
       loop:true,
       margin:10,
-      nav:true,
       autoplay: 10000,
       items: 1,
       autoHeight: true
